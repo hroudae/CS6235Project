1d63e8bcb8a579140c83a72ff041e709ac250ee0
Jeff Kelso <>
2021-04-06 13:30:24 -0600

    working on test. issues w/ encrypt matching expected output

--- test.c
+++ test.c
@@ -7,6 +7,69 @@
 #include "AES_lib.h"
 #include "constants.h"
 
+
+
+void printState(state_t state) {
+    int i, j;
+    for (i = 0; i < 4; i++) {
+        for (j = 0; j < 4; j++) {
+            printf("%02x ", state[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void AES_Encrypt(unsigned char* plainText, unsigned char* cipherText, uint32_t* roundKeys, AESVersion_t vers, int charCount) {
+    unsigned int numround = 0;
+
+    switch(vers) {
+        case AES128_VERSION:
+            numround = AES128_ROUNDS;
+            break;
+        case AES192_VERSION:
+            numround = AES192_ROUNDS;
+            break;
+        case AES256_VERSION:
+            numround = AES256_ROUNDS;
+            break;
+        default:
+            numround = 0;
+    }
+
+    unsigned int blocks = (charCount + (BLOCK_SIZE_BITS / 8)-1) / (BLOCK_SIZE_BITS / 8);
+
+    int i;
+    for (i = 0; i < blocks; i++) {
+        AES_Encrypt_Block(plainText+i*(BLOCK_SIZE_BITS / 8), cipherText+i*(BLOCK_SIZE_BITS / 8), roundKeys, numround);
+    }
+}
+
+void AES_Decrypt(unsigned char* cipherText, unsigned char* plainText, uint32_t* roundKeys, AESVersion_t vers, int charCount) {
+    unsigned int numround = 0;
+
+    switch(vers) {
+        case AES128_VERSION:
+            numround = AES128_ROUNDS;
+            break;
+        case AES192_VERSION:
+            numround = AES192_ROUNDS;
+            break;
+        case AES256_VERSION:
+            numround = AES256_ROUNDS;
+            break;
+        default:
+            numround = 0;
+    }
+
+    unsigned int blocks = (charCount + (BLOCK_SIZE_BITS / 8)-1) / (BLOCK_SIZE_BITS / 8);
+
+    int i;
+    for (i = 0; i < blocks; i++) {
+        AES_Decrypt_Block(cipherText+i*(BLOCK_SIZE_BITS / 8), plainText+i*(BLOCK_SIZE_BITS / 8), roundKeys, numround);
+    }
+}
+
+
 //Stuff for Reading Test Files
 //Takes a line of test file to turn into NUM of 8hex vals
 void parseKey(char* hex, uint32_t* out[], int outLength){
@@ -31,10 +94,10 @@ void parseKey(char* hex, uint32_t* out[], int outLength){
 
    for (int i = 0; i < outLength; i++) {
        strncpy(subkey, idx, 8);
-       //printf("Subkey: %s\n", subkey);
+       printf("Subkey: %s\n", subkey);
        out[i] = (uint32_t)strtoul(subkey, NULL, 16);
        idx += 8;
-       //printf("out[%d] %08x \n", i, out[i]);
+       printf("out[%d] %08x \n", i, out[i]);
    }
 }
 
@@ -85,10 +148,20 @@ int main(int argc, char* argv[]){
     unsigned int keysize = AES192_KEYSIZE;
 
     uint32_t *key[keysize]; // = malloc(sizeof(uint32_t)*6);
+
+    AESVersion_t version = AES192_VERSION;
+    NumRounds_t rounds = AES192_ROUNDS;
+    uint32_t *roundKeys = malloc(sizeof(uint32_t) * (4*rounds));
+
+    unsigned char cipherText[16] = {0};
+    unsigned char decryptPlainText[16] = {0};
+
+
     // uint32_t *plaintext[4];
     // uint32_t *cipher[4];
-    unsigned char *plaintext[16];
-    unsigned char *cipher[16];
+    unsigned char *plaintext[16]; //Original Plaintext
+    unsigned char *ciphertext[16]; //Ciphertext Tested
+    unsigned char *cipherknown[16]; //known Answer
 
     int flipper = -1;
 
@@ -96,22 +169,21 @@ int main(int argc, char* argv[]){
       switch(line[0]) {
           case 'K':
              printf("%s", line);
+             flipper = -1;
              parseKey(line, key, keysize); //Functional But hardcoded
              break;
           case 'P':
              printf("%s", line);
              parseLine(line, plaintext, 16); //Functional But hardcoded
-
-
              break;
           case 'C':
              if(line[1]== 'I'){
                printf("%s", line);
-               parseLine(line, cipher, 16); //Functional But hardcoded
-               flipper = 1;
-
-             }
-             else {
+               parseLine(line, cipherknown, 16); //Functional But hardcoded
+               printf("\n Cipher Text: ");
+               for (int i = 0; i < 16; i++) {
+                   printf("%x", cipherknown[i]);
+               }
                flipper = 1;
              }
              break;
@@ -119,6 +191,23 @@ int main(int argc, char* argv[]){
 
       if (flipper == 1){
         //encrypt
+        KeyExpansion(key, roundKeys, version);
+        AES_Encrypt(plaintext, ciphertext, roundKeys, version, 16);
+
+        printf("\nInput: ");
+        for (int i = 0; i < 16; i++) {
+            printf("%x", plaintext[i]);
+        }
+
+        printf("\nOutput: ");
+        for (int i = 0; i < 16; i++) {
+            printf("%x", ciphertext[i]);
+        }
+        printf("\nExpected Text: ");
+        for (int i = 0; i < 16; i++) {
+            printf("%x", cipherknown[i]);
+        }
+        printf("\n End Test \n");
 
         //check answer
 
