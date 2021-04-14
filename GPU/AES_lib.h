9479878c7112f43fe46b43d91f07aa70aca4d3e4
Evan Hrouda <u1302032@kingspeak2.wasatch.peaks>
2021-04-14 11:12:26 -0600

    CTR mode on GPU

--- GPU/AES_lib.h
+++ GPU/AES_lib.h
@@ -9,6 +9,8 @@
 
 
 void KeyExpansion(uint32_t* key, uint32_t* roundKeys, AESVersion_t vers);
+int GetIV(uint8_t *iv);
+extern __device__ void incrementCounter(uint8_t *new_ctr, uint8_t *ctr, int inc);
 extern __device__ void AES_Encrypt_Block(uint8_t* plainText, uint8_t* cipherText, uint32_t* roundKeys, unsigned int numround);
 extern __device__ void AES_Decrypt_Block(uint8_t* cipherText, uint8_t* plainText, uint32_t* roundKeys, unsigned int numround);
 
